1cab5c904e1e9bda15f8e7f617503d577f9ea75c
Osama Medhat <ososmedhat1@gmail.com>
2025-02-20 14:10:14 +0200

    setup coreUI

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import "./bootstrap";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot } from "react-dom/client";
+// import "@coreui/coreui/dist/css/coreui.min.css";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 

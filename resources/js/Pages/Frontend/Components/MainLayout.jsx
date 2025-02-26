63c97f67be54e90278c77bacf833e7f71a639306
Osama Medhat <ososmedhat1@gmail.com>
2025-02-26 21:24:16 +0200

    Navbar

--- resources/js/Pages/Frontend/Components/MainLayout.jsx
+++ resources/js/Pages/Frontend/Components/MainLayout.jsx
@@ -1,73 +1,8 @@
 import { useEffect } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import Footer from "./Footer";
-import NavList from "./Navbar";
-import ColorSchemesExample from "./Navbar";
+import NavBar from "./Navbar";
 
-// const MainLayout = () => {
-//     useEffect(() => {
-//         const loadScript = (src) => {
-//             return new Promise((resolve, reject) => {
-//                 const script = document.createElement("script");
-//                 script.src = src;
-//                 script.async = false; // Important: Ensures scripts load in order
-//                 script.onload = resolve;
-//                 script.onerror = reject;
-//                 document.body.appendChild(script);
-//             });
-//         };
-
-//         const loadScripts = async () => {
-//             try {
-//                 // Load critical scripts first in order
-//                 await loadScript("assets/js/jquery.min.js");
-//                 await loadScript("assets/js/jquery-migrate-3.0.1.min.js");
-//                 await loadScript("assets/js/popper.min.js");
-//                 await loadScript("assets/js/bootstrap.min.js");
-
-//                 // Now load the rest
-//                 const otherScripts = [
-//                     "assets/js/jquery.easing.1.3.js",
-//                     "assets/js/jquery.waypoints.min.js",
-//                     "assets/js/jquery.stellar.min.js",
-//                     "assets/js/owl.carousel.min.js",
-//                     "assets/js/jquery.magnific-popup.min.js",
-//                     "assets/js/aos.js",
-//                     "assets/js/jquery.animateNumber.min.js",
-//                     "assets/js/bootstrap-datepicker.js",
-//                     "assets/js/jquery.timepicker.min.js",
-//                     "assets/js/scrollax.min.js",
-//                     "https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false",
-//                     "assets/js/google-map.js",
-//                     "assets/js/main.js",
-//                 ];
-
-//                 for (const src of otherScripts) {
-//                     await loadScript(src);
-//                 }
-//             } catch (error) {
-//                 console.error("Script loading failed:", error);
-//             }
-//         };
-
-//         loadScripts();
-
-//         return () => {
-//             const scripts = document.querySelectorAll("script[src]");
-//             scripts.forEach((script) => document.body.removeChild(script));
-//         };
-//     }, []);
-
-//     return (
-//         <div>
-//             <Header />
-//             <Navbar />
-//             <Home />
-//         </div>
-//     );
-// };
-
-// export default MainLayout;
 const MainLayout = ({ children }) => {
     useEffect(() => {
         const loadScript = (src) => {
@@ -153,7 +88,8 @@ const MainLayout = ({ children }) => {
                 <link rel="stylesheet" href="assets/css/style.css" />
             </Helmet>
 
-            <ColorSchemesExample />
+            <NavBar />
+
             {/* Page Content */}
             <div>{children}</div>
 

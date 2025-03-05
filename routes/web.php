6cde16261990a08b39a3e6a4a35a2e2373317c83
Osama Medhat <ososmedhat1@gmail.com>
2025-03-05 02:53:34 +0200

    cleaned the routes

--- routes/web.php
+++ routes/web.php
@@ -6,41 +6,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+// Public Routes
 Route::get('/', function () {
-    return inertia('Frontend/Components/Header', [
+    return inertia('Frontend/Home', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
-// ->name("home");
-
-Route::get('/dashboard', function () {
-    if (Auth::check()) {
-        if (Auth::user()->role === 'admin') {
-            return redirect()->route('admin.dashboard');
-        }
-    }
-    // return inertia('User/Dashboard');
-    return inertia('Frontend/Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-//Admin Route
-Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-    Route::get('/dashboard/users', [AdminController::class, 'usersList'])->name('admin.users');
-    Route::delete('/dashboard/users/{id}', [AdminController::class, 'deleteUser'])->name('admin.users.delete');
-    // Route::get('admin/dashboard/users/{id}',[AdminController::class, 'user'])->name('admin.user');
-});
-
+})->name("home");
 
 Route::get("/home", function () {
     return inertia("Frontend/Home", [
@@ -48,6 +22,34 @@ Route::get("/home", function () {
             'user' => Auth::user(),
         ],
     ]);
-})->name("home");
+});
 
+// User Routes
+Route::middleware(['auth', 'verified'])->group((function () {
+
+    Route::get('/dashboard', function () {
+        if (Auth::check() && Auth::user()->role === "admin") {
+            return redirect()->route('admin.dashboard');
+        }
+    })->name('dashboard');
+
+    //Profile Routes
+    Route::prefix('profile')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+}));
+
+//Admin Route
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/users', [AdminController::class, 'usersList'])->name('admin.users');
+        Route::delete('/users/{id}', [AdminController::class, 'deleteUser'])->name('admin.users.delete');
+    });
+});
+
+// Auth Routes
 require __DIR__ . '/auth.php';
